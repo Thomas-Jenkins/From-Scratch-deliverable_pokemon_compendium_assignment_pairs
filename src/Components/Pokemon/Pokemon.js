587b1a20b42a5f3f610003a4d364587b9aefeb7b
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-09-22 09:21:58 -0700

    add css color pallette to pokemon cards

--- src/Components/Pokemon/Pokemon.js
+++ src/Components/Pokemon/Pokemon.js
@@ -3,7 +3,7 @@ import './Pokemon.css';
 
 export default function Pokemon({ pokemon, url_image, type_1, type_2 }) {
   return (
-    <div className="poke-card">
+    <div className={`poke-card ${type_1}`} >
       <h1 className="title">{pokemon}</h1>
       <img className="poke-img" src={url_image}></img>
       <div className="poke-types">
